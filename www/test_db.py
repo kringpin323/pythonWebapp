8d1832695455e785fa92b633ea803498244f75fb
Kingpin <kringpin_lin@163.com>
2015-01-16 01:44:54 +0800

    finish day04 model module

---
+++ www/test_db.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'David Lin'
+
+import logging
+
+from models import User, Blog, Comment
+
+from transwarp import db
+
+logging.basicConfig(level=logging.DEBUG)
+
+db.create_engine(user='root', password='Linux818', database='test')
+
+u = User(name='Test', email='test@example.com', password='1234567890', image='about:blank')
+
+# u.save()
+u.insert() # you must build the table first ! 
+
+print 'new user id:', u.id
+
+u1 = User.find_first('where email=?', 'test@example.com')
+print 'find user\'s name:', u1.name
+
+u1.delete()
+
+u2 = User.find_first('where email=?', 'test@example.com')
+print 'find user:', u2
