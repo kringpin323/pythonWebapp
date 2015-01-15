afb408ac52f417cf84d89e1e775ab9d70d5c6912
Kingpin <kringpin_lin@163.com>
2015-01-15 12:17:46 +0800

    orm.py metaclass and cls object

---
+++ www/transwarp/orm.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'David Lin'
+
+'''
+Database operation module. This module is independent with web module.
+'''
+
+import time, logging
+
+import db # import the file we write named db
+
+class Field(object): # Database entity instance Field
+
+	_count = 0
+
+	def __init__(self, **kw):
+		self.name = kw.get('name',None)
+		self._default = kw.get('default',None)
+		self.primary_key = kw.get('primary_key', False)
+		self.nullable = kw.get('nullable',False) # allow null
+		self.updatable = kw.get('updatable', True)
+		self.insertable = kw.get('insertable', True) # Field insertable
+		self.ddl = kw.get('ddl','')
+		self.order = Field._count    # _count for order
+		Field._count = Field._count + 1
+
+	@property
+	def default(self):
+		d = self._default
+		return d() if callable(d) else d
+
+	def __str__(self):
+		s = ['<%s:%s,%s,default(%s),' % (self.__class__.__name__, self.name, self.ddl, self._default)]
+		self.nullable and s.append('N')
+		self.updatable and s.append('U') # if self.updatable then s.append('U')
+		self.insertable and s.append('I')
+		s.append('>')
+		return ''.join(s)
+
+class StringField(Field):
+
+	def __init__(self, **kw):
+		if not 'default' in kw:
+			kw['default'] = '' # default values for this Field
+		if not 'ddl' in kw:
+			kw['ddl'] = 'varchar(255)'
+		super(StringField, self).__init__(**kw)
+
+class IntegerField(Field):
+
+	def __init__(self, **kw):
+		if not 'default' in kw:
+			kw['default'] = 0
+		if not 'ddl' in kw:
+			kw['ddl'] = 'bigint' # data type
+ 		super(IntegerField, self).__init__(**kw)
+
+class BooleanField(Field):
+
+ 	def __init__(self, **kw):
+ 		if not 'default' in kw:
+ 			kw['default'] = False
+ 		if not 'ddl' in kw:
+ 			kw['ddl'] = 'bool'
+ 		super(BooleanField, self).__init(**kw)
+
+class FloatField(Field):
+
+ 	def __init__(self, **kw):
+ 		if not 'default' in kw:
+ 			kw['default'] = 0.0
+ 		if not 'ddl' in kw:
+ 			kw['ddl'] = 'real'
+ 		super(BooleanField, self).__init(**kw)
+
+class TextField(Field):
+
+ 	def __init__(self, **kw):
+ 		if not 'default' in kw:
+ 			kw['default'] = ''
+ 		if not 'ddl' in kw:
+ 			kw['ddl'] = 'text'
+ 		super(BooleanField, self).__init(**kw)
+
+class BlobField(Field):
+
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = ''
+        if not 'ddl' in kw:
+            kw['ddl'] = 'blob'
+        super(BlobField, self).__init__(**kw)
+
+class VersionField(Field):
+
+    def __init__(self, name=None): # why so special ?
+    	super(VersionField, self).__init__(name=name, default=0, ddl='bigint')
+
+_triggers = frozenset(['pre_insert','pre_update','pre_delete'])  # three trigger
+
+def _gen_sql(table_name, mappings):
+	pass
+
+class ModelMetaclass(type):
+	'''
+	Metaclass for model objects, type replace object.
+	'''
+	def __new__(cls, name, bases, attrs):
+		# cls : class instance 
+		# name : subclass name
+		# bases: superclass collections
+		# attrs: method or attribute of class instance
+
+		#skip base Model class:
+		if name=='Model': 
+			return type.__new__(cls, name, bases, attrs)
+
+		# subclass which extends Model
+		# store all subclasses info: still don't get it
+		if not hasattr(cls, 'subclasses'): # hold subclasses 
+			cls.subclasses = {}
+		if not name in cls.subclasses:
+			cls.subclasses[name] = name
+		else:
+			logging.warning('Redefine class: %s' % name)
+
+		logging.info('Scan ORMapping %s...' % name)
+		mappings = dict()
+		primary_key = None
+		for k, v in attrs.iteritems():
+			if isinstance(v, Field):
+				if not v.name:
+					v.name = k   # attrs' value and name
+				logging.info('Found mapping: %s => %s' %(k,v))
+				# check duplicate primary key:
+				if v.primary_key:
+					if primary_key:
+						raise TypeError('Cannot define more than 1 primary key in class: %s' % name)
+					if v.updatable:
+						logging.warning('NOTE: change primary key to non-updatable. ')
+						v.updatable = False
+					if v.nullable:
+						logging.warning('Note: change primary key to non-nullable.')
+						v.nullable = False
+					primary_key = v
+				mappings[k] = v
+	# check exist of primary key:
+	if not primary_key:
+		raise TypeError('Primary key not defined in class: %s' % name)
+	for k in mappings.iterkeys():
+		attrs.pop(k)  # redefine k 
+	if not '__table__' in attrs:
+		attrs['__table__'] = name.lower() # __table__ is class name
+	attrs['__mappings__'] = mappings  # all those mapping
+	attrs['__primary_key__'] = primary_key
+	attrs['__sql__'] = lambda self: _gen_sql(attrs['__table__'], mappings)
+	for trigger in _triggers:
+		if not trigger in attrs:
+			attrs[trigger] = None
+	return type.__new__(cls, name, bases, attrs)		
+
+class Model(dict):
+	__metaclass__ = ModelMetaclass
+
+	def __init__(self, **kw):
+		super(Model, self).__init__(**kw)
+
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+	def __setattr__(self, key, value):
+		self[key] = value
+
+	@classmethod
+	def get(cls, pk):
+		'''
+		Get by primary key.
+		'''
+		d = db.select_one('select * from %s where %s=?' % (cls.__table__, cls.__primary_key__.name),pk)
+		return cls(**d) if d else None
+
+	@classmethod
+	def find_first(cls, where, *args):
+		'''
+		Find by where clause
+		'''
+		d = db.select_one('select * from %s %s' % (cls.__table__, where), *args)
+		return cls(**d) if d else None
+
+	@classmethod
+	def find_all(cls, *args):
+		'''
+		Find all and return list.
+		'''
+		L = db.select('select * from `%s`' % cls.__table__)
+		return [cls(**d) for d in L]
+
+
+
+
+
