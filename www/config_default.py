d7ded04a205128bc6def3513e42bb81b0bf8ee68
Kingpin <kringpin_lin@163.com>
2015-01-25 16:19:47 +0800

    day06 config

---
+++ www/config_default.py
@@ -0,0 +1,21 @@
+#!/urs/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'David Lin'
+
+'''
+Default configurations.
+'''
+
+configs = {
+	'db': {
+		'host':'127.0.0.1',
+		'port': 3306,
+		'user': 'root'
+		'password':  'Linux818'
+		'database': 'test'
+	},
+	'session': {
+		'secret': 'AwEsOmE' # not get it
+	}
+}
